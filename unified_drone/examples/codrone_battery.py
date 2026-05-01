eaf618313fa14c65b85bb1df522c123caf11360a
alejadelg <2024214517@gnu.ac.kr.com>
2026-05-01 22:33:55 +0900

    Add codrone examples (basic, battery, forward)

---
+++ unified_drone/examples/codrone_battery.py
@@ -0,0 +1,9 @@
+from codrone_edu.drone import *
+
+drone = Drone()
+drone.pair()
+
+battery = drone.get_battery()
+print(battery)
+
+drone.close()
---
+++ unified_drone/examples/codrone_forward.py
@@ -0,0 +1,11 @@
+from codrone_edu.drone import *
+import time
+
+drone = Drone()
+drone.pair()
+
+drone.takeoff()
+drone.move_forward(distance=50, units="cm", speed=1)
+drone.land()
+
+drone.close()
